c7c8c5ca3f1306509b08a7f8a7f1d136cd0fde06
Santiago <ithurralde.santiago@gmail.com>
2024-03-12 13:14:41 -0300

    int: Change the Groovy build.gradle to Kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    // Apply the application plugin to add support for building a CLI application in Java.
+    id("application")
+}
+
+repositories {
+    maven("https://dl.equo.dev/middleware/mvn/release")
+    maven("https://dl.equo.dev/chromium-swt-ee/equo-gpl/mvn")
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    implementation("dev.equo:dev.equo.sdk:0.0.1")
+}
