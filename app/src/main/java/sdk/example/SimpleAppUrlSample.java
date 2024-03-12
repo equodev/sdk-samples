5fcf8aa7350ebe7cab97732c65856c56a3574a63
Santiago <ithurralde.santiago@gmail.com>
2024-03-12 18:48:15 -0300

    int: Add new sample app to launch URL provided as a String

---
+++ app/src/main/java/sdk/example/SimpleAppUrlSample.java
@@ -0,0 +1,13 @@
+package sdk.example;
+
+import com.equo.application.client.EquoApp;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SimpleAppUrlSample {
+    public static void main(String[] args) throws MalformedURLException {
+        EquoApp.create("SIMPLE-URL-APP")
+                .launch("https://equo.dev");
+    }
+}
