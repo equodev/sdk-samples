9bb11cf177ea26b9e71625b6b89f9a0b299df8d9
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-03-18 19:32:44 -0300

    refactor: Remove unused import statements

--- app/src/main/java/sdk/example/SimpleAppUrlSample.java
+++ app/src/main/java/sdk/example/SimpleAppUrlSample.java
@@ -2,9 +2,6 @@ package sdk.example;
 
 import com.equo.application.client.EquoApp;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 public class SimpleAppUrlSample {
     public static void main(String[] args) {
         EquoApp.create("SIMPLE-URL-APP")
