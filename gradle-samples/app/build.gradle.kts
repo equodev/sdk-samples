c9bd37341966aedd10b432822dc6d852bab583f5
Santiago <ithurralde.santiago@gmail.com>
2024-04-08 17:18:10 -0300

    Add Equo Chromium as aptional based on operative system and architecture

--- gradle-samples/app/build.gradle.kts
+++ gradle-samples/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     id("application")
+    id("com.google.osdetector") version "1.7.3"
 }
 
 repositories {
@@ -10,6 +11,13 @@ repositories {
     mavenCentral()
 }
 
+val sanitizeArchName = osdetector.arch.replace("_", "")
+
 dependencies {
     implementation("dev.equo:dev.equo.sdk:0.0.2")
+    runtimeOnly("dev.equo:dev.equo.sdk:0.0.2") {
+        capabilities {
+            requireCapability("dev.equo:core-chromium-${sanitizeArchName}-${osdetector.os}")
+        }
+    }
 }
--- gradle-samples/osgi-app/build.gradle.kts
+++ gradle-samples/osgi-app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     id("application")
+    id("com.google.osdetector") version "1.7.3"
 }
 
 repositories {
@@ -10,11 +11,18 @@ repositories {
     mavenCentral()
 }
 
+val sanitizeArchName = osdetector.arch.replace("_", "")
+
 dependencies {
     implementation("dev.equo:dev.equo.sdk:0.0.2")
     runtimeOnly("dev.equo:dev.equo.sdk:0.0.2") {
         capabilities {
             requireCapability("dev.equo:core-osgi-support")
+        }
+    }
+    runtimeOnly("dev.equo:dev.equo.sdk:0.0.2") {
+        capabilities {
+            requireCapability("dev.equo:core-chromium-${sanitizeArchName}-${osdetector.os}")
         }
     }
 }
